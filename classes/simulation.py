48d8ac608abe3510deb5764cbdb26e2aa2ce9188
Felipe <wowaccount123555@hotmail.com>
2016-09-19 21:22:38 -0300

    Making simulation.py code cleaner

--- classes/simulation.py
+++ classes/simulation.py
@@ -1,45 +1,55 @@
 from FGAme import *
 from rope import Rope
-import math
 
-MOVING = True
+MOVING = False
+CONSTANT_K = 10
+MAX_DIST = 200
 
-world.add.margin(10)
-player = world.add.circle(10, pos=pos.middle)
-platform = world.add.circle(30, pos=pos.middle+(0, 200))
+ROPE = None
+PLAYER = None
+PLATFORM = None
 
-rope = Rope(starting_position=platform.pos, \
-			ending_position=player.pos, \
-			balance_center=pos.middle-(0,300))
+def start():
+    global ROPE
+    global PLAYER
+    global PLATFORM
 
-player.gravity = 30
-k = 10
-player.damping = 1
-dist_max = 200
-if(MOVING):
-	platform.vel = (100, -30)
+    world.add.margin(10)
+    PLAYER = world.add.circle(10, pos=pos.middle)
+    PLATFORM = world.add.circle(30, pos=pos.middle+(0, 200))
+
+    ROPE = Rope(starting_position=PLATFORM.pos, \
+                ending_position=PLAYER.pos, \
+                balance_center=pos.middle-(0, 300))
+
+    PLAYER.gravity = 30
+    PLAYER.damping = 1
+    if MOVING:
+        PLATFORM.vel = (100, -30)
+    run()
+
 
 @listen('frame-enter')
 def elastic_force():
-	global rope
+    global ROPE
+    if ROPE != None:
+        ROPE.remove()
+        ROPE = Rope(starting_position=PLATFORM.pos, \
+                    ending_position=PLAYER.pos, \
+                    balance_center=Vec(PLATFORM.center.x, \
+                                       PLATFORM.center.y-MAX_DIST))
 
-	rope.remove()
-	rope = Rope(starting_position=platform.pos, \
-				ending_position=player.pos, \
-				balance_center=Vec(platform.center.x, platform.center.y-dist_max))
+        dist = PLATFORM.pos - PLAYER.pos
+        direction = dist - dist.normalize()*MAX_DIST
+        direction *= CONSTANT_K
+        PLAYER.apply_force(direction, 1)
+    else:
+        #Do nothing
+        pass
 
-	dist = platform.pos - player.pos
-	direction = dist - dist.normalize()*dist_max
-	
-	direction *= k	
-	player.apply_force(direction, 1)
-
-@listen('long-press', 'left')
-def wind():
-	player.apply_force((-50, 0), 1)
-
-@listen('long-press', 'right')
-def wind2():
-	player.apply_force((50, 0), 0.5)
-
-run()
+@listen('long-press', 'left', dy=50)
+def windleft(dy):
+    PLAYER.apply_force((-dy, 0), 1)
+@listen('long-press', 'right', dy=50)
+def windright(dy):
+    PLAYER.apply_force((dy, 0), 0.5)
